4f67e39eefe3c95b1cc2cd4d15eebab6f5edcfca
Eren Kulan <kulaneren@gmail.com>
2023-11-17 12:29:47 +0000

    Workspace, app, api module creation

---
+++ ChatAPI/ChatAPI/ChatAPI.h
@@ -0,0 +1,18 @@
+//
+//  ChatAPI.h
+//  ChatAPI
+//
+//  Created by eren  on 17/11/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ChatAPI.
+FOUNDATION_EXPORT double ChatAPIVersionNumber;
+
+//! Project version string for ChatAPI.
+FOUNDATION_EXPORT const unsigned char ChatAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChatAPI/PublicHeader.h>
+
+
